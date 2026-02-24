41e456cf316cffdb64c33c80c8087433dadb9f64
derkiecheverria <mdcderki@gmail.com>
2026-02-24 13:24:13 -0500

    Initial Commit

---
+++ InternTracker/InternTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  InternTrackerApp.swift
+//  InternTracker
+//
+//  Created by Derki Echevarria Rodriguez on 2/24/26.
+//
+
+import SwiftUI
+
+@main
+struct InternTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
